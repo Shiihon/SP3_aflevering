446594413175f0d0a4abff6b6c98b4e3e13140ef
Shiihon <144337411+Shiihon@users.noreply.github.com>
2023-11-09 10:58:45 +0100

    SP3

---
+++ src/main/java/org/SP3/FileIO.java
@@ -0,0 +1,12 @@
+package org.SP3;
+ import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.FileReader;
+import java.util.ArrayList;
+
+public class FileIO {
+    public static ArrayList <String> loadData (String path){
+        FileReader reader = new FileReader(cars.json)
+    }
+}
